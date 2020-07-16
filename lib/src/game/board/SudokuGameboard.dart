4694994e23ae9bc066743ca94af664f0356202a7
Spencer <spencer.carlson@student.csulb.edu>
2020-07-16 12:16:38 -0700

    Move column and row getters to the game board class, rename methods, organize imports

--- lib/src/game/board/SudokuGameboard.dart
+++ lib/src/game/board/SudokuGameboard.dart
@@ -14,13 +14,55 @@ class SudokuGameboard {
     for (var y = 0; y < 3; y ++) {
       for (var x = 0; x < 3; x++) {
         unshuffledMap[Pair(x, y)] = SudokuChunk.fromSubList(
-            getChunkData(generator.numbers, (x * 3), (y * 3)));
+            chunkValuesAt(generator.numbers, (x * 3), (y * 3)));
       }
     }
     _chunkMap.addAll(unshuffledMap);
   }
 
-  List<Pair> createColumnPairList(int index) {
+  /// Given a column index, return the Integer values of the column.
+  List<int> column(int col) {
+    var colValues = <int>[];
+
+    var gameboardCol = getEquivalentBoardIndex(col);
+    var chunkCol = getChunkIndex(col);
+
+    var pairsOfChunks = columnPairList(gameboardCol);
+    var pairsOfSquares = columnPairList(chunkCol);
+
+    final chunkList = pairsOfChunks.map((pair) =>
+    sudokuChunks[pair]).toList();
+
+    for(var chunk in chunkList) {
+      colValues.addAll(
+          pairsOfSquares.map((pair) =>
+          chunk.sudokuSquares[pair].value));
+    }
+    return colValues;
+  }
+
+  /// Given a row index, return the Integer values of the row.
+  List<int> row(int row) {
+    var rowValues = <int>[];
+
+    var gameboardRow = getEquivalentBoardIndex(row);
+    var chunkRow = getChunkIndex(row);
+
+    var pairsOfChunks = rowPairList(gameboardRow);
+    var pairsOfSquares = rowPairList(chunkRow);
+
+    final chunkList = pairsOfChunks.map((pair) =>
+    sudokuChunks[pair]).toList();
+
+    for(var chunk in chunkList) {
+      rowValues.addAll(
+          pairsOfSquares.map((pair) =>
+          chunk.sudokuSquares[pair].value));
+    }
+    return rowValues;
+  }
+  /// Given a column index, 1 - 9, create a List of Pairs that compose the given column.
+  List<Pair> columnPairList(int index) {
     var pairs = <Pair>[];
     for(var a = 0; a < 3; a++) {
       pairs.add(Pair(index, a));
@@ -28,7 +70,8 @@ class SudokuGameboard {
     return pairs;
   }
 
-  List<Pair> createRowPairList(int index) {
+  /// Given a row index, 1 - 9, create a List of Pairs that compose the given row.
+  List<Pair> rowPairList(int index) {
     var pairs = <Pair>[];
     for(var a = 0; a < 3; a++) {
       pairs.add(Pair(a, index));
@@ -36,7 +79,9 @@ class SudokuGameboard {
     return pairs;
   }
 
-  List<int> getChunkData(var list, var x, var y) {
+  /// Given a List of Integers representing a game board of 9 chunks, return
+  /// a specific chunk given x and y are their origin coordinates (0, 0).
+  List<int> chunkValuesAt(var list, var x, var y) {
     var chunkData = <int>[];
     for(var a = 0; a < 3; a++) {
       chunkData.addAll(list[y + a].sublist(x, x + 3));
@@ -44,8 +89,24 @@ class SudokuGameboard {
     return chunkData;
   }
 
-  int getBoardChunkIndex(int index) {
-    return ((index / 3) - 1).ceil();
+  ///Given a particular row/column index, 1 - 9, return the equivalent index
+  ///within the specific chunk.
+  int getChunkIndex(int index) {
+    var result = (index % 3) - 1;
+    if(result == -1) {
+      return 2;
+    }
+    return result;
+  }
+
+  ///Given a particular row/column index, 1 - 9, return the row/column index
+  ///where it's contained within the game board.
+  int getEquivalentBoardIndex(int index) {
+    var result = ((index / 3) - 1).ceil();
+    if(result == -1) {
+      return 0;
+    }
+    return result;
   }
 
   Map<Pair, SudokuChunk> get sudokuChunks => _chunkMap;
--- lib/src/game/logic/GameManager.dart
+++ lib/src/game/logic/GameManager.dart
@@ -1,5 +1,6 @@
-import 'package:sudoku/src/game/board/Pair.dart';
 import 'package:collection/collection.dart';
+
+import 'package:sudoku/src/game/board/Pair.dart';
 import 'package:sudoku/src/game/board/SudokuGameboard.dart';
 
 ///The GameManager class handles the state of the Sudoku game.
@@ -19,89 +20,12 @@ class GameManager {
 
   bool checkForCompletion() {
     for(var a = 0; a < board.sudokuChunks.values.length; a++) {
-      if(!validSelection(getRow(a).toList()) ||
-          !validSelection(getColumn(a).toList()) ||
-          !validSelection(board.sudokuChunks.values.elementAt(a).toList())) {
+      if(!validSelection(board.row(a).toList())
+          || !validSelection(board.column(a).toList())
+          || !validSelection(board.sudokuChunks.values.elementAt(a).toList())) {
         return false;
       }
     }
-
     return true;
-  }
-
-  List<int> getColumn(int col) {
-    var colValues = <int>[];
-
-    var gameboardCol = getEquivalentBoardIndex(col);
-    var chunkCol = getChunkIndex(col);
-
-    var pairsOfChunks = createColumnPairList(gameboardCol);
-    var pairsOfSquares = createColumnPairList(chunkCol);
-
-    final chunkList = pairsOfChunks.map((pair) =>
-    _board.sudokuChunks[pair]).toList();
-
-    for(var chunk in chunkList) {
-      colValues.addAll(
-          pairsOfSquares.map((pair) =>
-          chunk.sudokuSquares[pair].value));
-    }
-    return colValues;
-  }
-
-  List<int> getRow(int row) {
-    var rowValues = <int>[];
-
-    var gameboardRow = getEquivalentBoardIndex(row);
-    var chunkRow = getChunkIndex(row);
-
-    var pairsOfChunks = createRowPairList(gameboardRow);
-    var pairsOfSquares = createRowPairList(chunkRow);
-
-    final chunkList = pairsOfChunks.map((pair) =>
-    _board.sudokuChunks[pair]).toList();
-
-    for(var chunk in chunkList) {
-      rowValues.addAll(
-          pairsOfSquares.map((pair) =>
-          chunk.sudokuSquares[pair].value));
-    }
-    return rowValues;
-  }
-
-  ///Given a particular row/column index, 1 - 9, return the equivalent index
-  ///within the specific chunk.
-  int getChunkIndex(int index) {
-    var result = (index % 3) - 1;
-    if(result == -1) {
-      return 2;
-    }
-    return result;
-  }
-
-  ///Given a particular row/column index, 1 - 9, return the row/column index
-  ///where it's contained within the game board.
-  int getEquivalentBoardIndex(int index) {
-    var result = ((index / 3) - 1).ceil();
-    if(result == -1) {
-      return 0;
-    }
-    return result;
-  }
-
-  List<Pair> createColumnPairList(int index) {
-    var pairs = <Pair>[];
-    for(var a = 0; a < 3; a++) {
-      pairs.add(Pair(index, a));
-    }
-    return pairs;
-  }
-
-  List<Pair> createRowPairList(int index) {
-    var pairs = <Pair>[];
-    for(var a = 0; a < 3; a++) {
-      pairs.add(Pair(a, index));
-    }
-    return pairs;
   }
 }
